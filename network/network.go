c07bce090aaf1b1057ca8a59083d3465f5ec5841
guming <gumingcn@gmail.com>
2018-09-04 10:22:53 +0800

    fixed the bugs of network

--- network/network.go
+++ network/network.go
@@ -225,7 +225,7 @@ func Disconnect(networkName string, cinfo *container.ContainerInfo) error {
 func configIpAddrRouteForEndpoint(endpoint *Endpoint, cinfo *container.ContainerInfo) error {
 	pname:=endpoint.Device.PeerName
 	peerlink,_:=netlink.LinkByName(pname)
-	ipnet:=endpoint.Network.IpRange.String()
+	//ipnet:=endpoint.Network.IpRange.String()
 	defer enterContainerNetNS(&peerlink,cinfo)
 	interfaceIP := *endpoint.Network.IpRange
 	interfaceIP.IP = endpoint.IPAddress
